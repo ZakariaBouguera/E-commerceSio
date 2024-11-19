2abc3f7c4e0f6773ecc48689c7058c8314d62e48
ZakariaBouguera <146200319+ZakariaBouguera@users.noreply.github.com>
2024-11-19 14:36:31 +0100

    Création Controlleur Admin

---
+++ Ecommerce/routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/admin/connexion',[AdminControllers::class ,'connexion']);
+#Route::get('/admin/seconnecter',function (){
+#   return "Connecter vous";
+#});
+Route::get('/admin/connecter',[AdminControllers::class ,'connecter']);
+#Route::get('/admin/connecter',function (){
+#   return "Bienvenue admin";
+#});
+
+Route::get('/admin/ModificationPrix',[AdminControllers::class ,'ModificationPrix']);
+#Route::get('/admin/Modificationprix',function (){
+#    return "Vous pouvez modifier les prix ";
+#});
+
+Route::get('/admin/PromotionPrix',[AdminControllers::class ,'PromotionPrix']);
+#Route::get('/admin/Promotionprix',function (){
+#    return "Vous pouvez appliquer une promotion ";
+#});
+
+Route::get('/admin/Stock',[AdminControllers::class ,'Stock']);
+#Route::get('/admin/Stock',function (){
+#    return "Vous pouvez gerer le stock";
+#});
+
+Route::get('/admin/SeDeconnetcer',[AdminControllers::class ,'SeDeconnetcer']);
+#Route::get('/admin/sedeconnecter',function (){
+#   return "deconnecter vous ";
+#});
+
+
+Route::fallback(function(){
+ return redirect('/');
+
+});
